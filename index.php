613a7249be24c16828d460590e53f83370a67399
Raphael Cavalcanti <rc_peres@hotmail.com>
2021-01-13 14:49:45 -0300

    imagem trip

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
-<img src = https://www.satotrader.com.br/site/views/data/noticias/as-acoes-do-itau-itub3-itub4-a-hora-e-agora-image-1568988669.jpg >
+<img src = "https://www.satotrader.com.br/site/views/data/noticias/as-acoes-do-itau-itub3-itub4-a-hora-e-agora-image-1568988669.jpg" >
+<img src = "https://img.traveltriangle.com/blog/wp-content/uploads/2018/11/Cover-Fiji-In-Decemberepb0310.jpg" >
